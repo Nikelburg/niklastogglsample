9bdd974535a31718c43dca55cced77ab20824918
Tim Becker <tim@presseverykey.com>
2017-01-20 13:17:25 +0100

    onSparkLogin und onSparkLogin ausgelagert

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-var spark = require("spark"),
-	TogglClient = require("toggl-api"),
-	toggl = new TogglClient({apiToken: "9c6752b7992e7d41f0b72f03c0951b35"}),
-	_ = require("underscore"),
-	currentParticle;
+// imports of spark and toggle functionality
+var spark       = require("spark"),
+    TogglClient = require("toggl-api");
+
+// instatiate global toggle api client and particle.
+var toggl       = new TogglClient({apiToken: "9c6752b7992e7d41f0b72f03c0951b35"}),
+	  currentParticle;
+
+const MY_PARTICLE_NAME = "TogglBitch"
 
 initParticle();
+
+// utility print function to save us some typing.
 function p(msg) { console.log(msg) }
+
+// guard variable to avoid calling Toggle twice on double click.
 var amSpeakingWithToggle = false
-function initParticle() {
-	spark.on("login", function(err, body) {
-		console.log("Particle device login successful: ", body);
-		var deviceList = spark.listDevices();
 
-		deviceList.then(function(devices) {
-			var currentParticle = _.find(devices, function(device) {
-				return device.name == "TogglBitch";
-			});
-			
-			console.log("TogglBitch was found: ", currentParticle);
-
-			currentParticle.onEvent("buttonPressed", function() {
+function onSparkButtonPressed () {
 				console.log("Button was pressed!");
 				if (amSpeakingWithToggle) {
 					p("ignoring")
@@ -68,13 +65,38 @@ console.log("!!!!!!!!!!!!!!!!!!")
             });
           }
         });
-			});
-		});
-	});
+			}
 
-	spark.login({
-		accessToken: "9ab9ba7bcddbaf84ff559ef0820a7c05ae9c46ff"
-	}, function(err, body) {
-		if (!err) console.log("API login complete!");
-	});
+function onSparkLogin (err, body) {
+  p("Particle device login successful: ", body)
+  
+  var deviceListPromise = spark.listDevices();
+  // find my toggl Particle button ...
+  deviceListPromise.then( (devices) => {
+    for (var i = 0; i!=devices.length; ++i) {
+      if (devices[i].name === MY_PARTICLE_NAME) {
+        currentParticle = devices[i]
+
+        p("Particle was found: ", currentParticle);
+        // register to handle buttonPressEvents from Particle
+        currentParticle.onEvent("buttonPressed", onSparkButtonPressed);
+        break
+      } // if
+    } // for
+  } /* TODO write a reject procedure for unsuccessful listDevices calls.*/);
+			
+}
+
+function initParticle() {
+  spark.on("login", onSparkLogin);
+
+  spark.login({
+    accessToken: "9ab9ba7bcddbaf84ff559ef0820a7c05ae9c46ff"
+  }, (err, body) => {
+    if (!err) {
+      p(`API login complete! ${JSON.stringify(body)}`)
+    } else {
+      p("login failed: "+err)
+    }
+  }) // end spark.login
 }
